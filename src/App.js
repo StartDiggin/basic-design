031ee06db4b498286cbc8cfea710c4fac76c7f16
Johnny Gonzales <dimensions122@gmail.com>
2020-12-28 16:41:36 -0500

    grid setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import './css/style.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Basic Design</h1>
+      <div className="App--1">box-1 box-1 box-1</div>
+      <div className="App--2">box-2</div>
+      <div className="App--3">box-3</div>
+      <div className="App--4">box-4</div>
+      <div className="App--5">box-5</div>
+      <div className="App--6">box-6</div>
+      <div className="App--7">box-7</div>
+      <div className="App--8">does the same thing as auto-fill but collapses the columns with no content in them </div>
     </div>
   );
 }
